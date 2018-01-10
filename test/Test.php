ee5c2df5074d9882f094db372b0250768185b29a
Myroslav Zozulia <myroslav.zozulia@eleks.com>
2018-01-10 15:55:37 +0200

    added dummy test

---
+++ test/Test.php
@@ -0,0 +1,12 @@
+<?php
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class Test extends TestCase
+{
+    public function testDummy()
+    {
+        $this->assertTrue(true);
+    }
+}
